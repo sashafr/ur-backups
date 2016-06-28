b57351f0615f26d7e5cc5e6ba1b7f9ad5d0a3239
Britt Binler <sashafr@sas.upenn.edu>
2016-06-28 00:01:27 -0400

    fixed wget

--- backup-script-sample.sh
+++ backup-script-sample.sh
@@ -1,4 +1,5 @@
-wget -'www.example.com/export'
-git add *
+wget -O private_file_backup.json 'http://www.example.com/export/?entity=file&type=json&a=True&user=username&pw=password'
+wget -O file_backup.json 'http://www.example.com/export/?entity=file&type=json&a=False&user=username&pw=password'
+git add -A
 git commit -m "latest update"
-git push https://username:password@github.com/username/repository.git master
+git push origin master
