56e8df5995211873ab88e303d59065e061560f53
Michael Tedeschi <mike@interactivemechanics.com>
2016-07-05 03:14:31 -0400

    script updated for all types

--- backup-script-sample.sh
+++ backup-script-sample.sh
@@ -1,12 +1,34 @@
-wget -O private_file_backup.json 'http://www.example.com/export/?entity=file&type=json&a=True&user=username&pw=password'
-wget -O file_backup.json 'http://www.example.com/export/?entity=file&type=json&a=False&user=username&pw=password'
-wget -O private_file_backup.xml 'http://www.example.com/export/?entity=file&type=xml&a=True&user=username&pw=password'
-wget -O file_backup.xml 'http://www.example.com/export/?entity=file&type=xml&a=False&user=username&pw=password'
-wget -O private_file_backup.csv 'http://www.example.com/export/?entity=file&type=csv&a=True&user=username&pw=password'
-wget -O file_backup.csv 'http://www.example.com/export/?entity=file&type=csv&a=False&user=username&pw=password'
+wget -O private_file_backup.json 'http://www.ur-online.org/export/?entity=file&type=json&a=True&user=USERNAME&pw=PASSWORD'
+wget -O file_backup.json 'http://www.ur-online.org/export/?entity=file&type=json&a=False&user=USERNAME&pw=PASSWORD'
+wget -O private_file_backup.xml 'http://www.ur-online.org/export/?entity=file&type=xml&a=True&user=USERNAME&pw=PASSWORD'
+wget -O file_backup.xml 'http://www.ur-online.org/export/?entity=file&type=xml&a=False&user=USERNAME&pw=PASSWORD'
+wget -O private_file_backup.csv 'http://www.ur-online.org/export/?entity=file&type=csv&a=True&user=USERNAME&pw=PASSWORD'
+wget -O file_backup.csv 'http://www.ur-online.org/export/?entity=file&type=csv&a=False&user=USERNAME&pw=PASSWORD'
+wget -O private_object_backup.json "http://www.ur-online.org/export/?entity=subject&type=json&a=True&user=USERNAME&pw=PASSWORD" 
+wget -O object_backup.json "http://www.ur-online.org/export/?entity=subject&type=json&a=False&user=USERNAME&pw=PASSWORD"
+wget -O private_object_backup.xml "http://www.ur-online.org/export/?entity=subject&type=xml&a=True&user=USERNAME&pw=PASSWORD" 
+wget -O object_backup.xml "http://www.ur-online.org/export/?entity=subject&type=xml&a=False&user=USERNAME&pw=PASSWORD"
+wget -O private_object_backup.csv "http://www.ur-online.org/export/?entity=subject&type=csv&a=True&user=USERNAME&pw=PASSWORD"
+wget -O object_backup.csv "http://www.ur-online.org/export/?entity=subject&type=csv&a=False&user=USERNAME&pw=PASSWORD"
+wget -O private_location_backup.json "http://www.ur-online.org/export/?entity=location&type=json&a=True&user=USERNAME&pw=PASSWORD"
+wget -O location_backup.json "http://www.ur-online.org/export/?entity=location&type=json&a=False&user=USERNAME&pw=PASSWORD"
+wget -O private_location_backup.xml "http://www.ur-online.org/export/?entity=location&type=xml&a=True&user=USERNAME&pw=PASSWORD"
+wget -O location_backup.xml "http://www.ur-online.org/export/?entity=location&type=xml&a=False&user=USERNAME&pw=PASSWORD"
+wget -O private_location_backup.csv "http://www.ur-online.org/export/?entity=location&type=csv&a=True&user=USERNAME&pw=PASSWORD"
+wget -O location_backup.csv "http://www.ur-online.org/export/?entity=location&type=csv&a=False&user=USERNAME&pw=PASSWORD"
+wget -O private_media_backup.json "http://www.ur-online.org/export/?entity=media&type=json&a=True&user=USERNAME&pw=PASSWORD"
+wget -O media_backup.json "http://www.ur-online.org/export/?entity=media&type=json&a=False&user=USERNAME&pw=PASSWORD"
+wget -O private_media_backup.xml "http://www.ur-online.org/export/?entity=media&type=xml&a=True&user=USERNAME&pw=PASSWORD"
+wget -O media_backup.xml "http://www.ur-online.org/export/?entity=media&type=xml&a=False&user=USERNAME&pw=PASSWORD"
+wget -O private_media_backup.csv "http://www.ur-online.org/export/?entity=media&type=csv&a=True&user=USERNAME&pw=PASSWORD"
+wget -O media_backup.csv "http://www.ur-online.org/export/?entity=media&type=csv&a=False&user=USERNAME&pw=PASSWORD"
+wget -O private_people_backup.json "http://www.ur-online.org/export/?entity=people&type=json&a=True&user=USERNAME&pw=PASSWORD"
+wget -O people_backup.json "http://www.ur-online.org/export/?entity=people&type=json&a=False&user=USERNAME&pw=PASSWORD"
+wget -O private_people_backup.xml "http://www.ur-online.org/export/?entity=people&type=xml&a=True&user=USERNAME&pw=PASSWORD"
+wget -O people_backup.xml "http://www.ur-online.org/export/?entity=people&type=xml&a=False&user=USERNAME&pw=PASSWORD"
+wget -O private_people_backup.csv "http://www.ur-online.org/export/?entity=people&type=csv&a=True&user=USERNAME&pw=PASSWORD"
+wget -O people_backup.csv "http://www.ur-online.org/export/?entity=people&type=csv&a=False&user=USERNAME&pw=PASSWORD"
 
 git add -A
-
 git commit -m "latest update"
-
 git push origin master
